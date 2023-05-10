b2cd6c20744a5b25d81ecd29cfe5d141bf4922d9
Hưng <96417124+huqedgar@users.noreply.github.com>
2023-05-10 12:50:23 +0700

    Complete the project (development phase) - Fix bug

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,148 +1,119 @@
-import { useEffect } from 'react';
-import { gsap } from 'gsap';
+import React, { useState, memo, useCallback, useRef, useEffect } from 'react';
 import classNames from 'classnames/bind';
 import styles from './Banner.module.scss';
-import './Banner.css';
+import Button from '../../shared/Button/Button';
+import { NavLink } from 'react-router-dom';
 
 const cx = classNames.bind(styles);
 
-const Banner = () => {
+const countries = ['Vietnam'];
+const videoSources = Array(1)
+    .fill()
+    .map((_, i) => ({
+        src: require(`../../assets/videos/vid-${i + 1}.mp4`),
+    }));
+
+const Banner = memo(() => {
+    const [videos, setVideos] = useState(videoSources.map((source, i) => ({ src: source.src, isActive: i === 0 })));
+    const indexRef = useRef(0);
+
+    const showVideo = useCallback(
+        (nextIndex) => {
+            const newIndex = (nextIndex + videos.length) % videos.length;
+            setVideos((prevVideos) =>
+                prevVideos.map((video, i) => {
+                    const isPlaying = i === newIndex;
+                    const videoElement = document.getElementById(`video${i + 1}`);
+                    if (isPlaying) {
+                        videoElement.play();
+                    } else {
+                        videoElement.pause();
+                    }
+                    return {
+                        ...video,
+                        isActive: isPlaying,
+                    };
+                }),
+            );
+            indexRef.current = newIndex;
+        },
+        [videos],
+    );
+
     useEffect(() => {
-        let overlay = document.querySelector('.sectionSlider');
-        let i = 1;
-        let del = 3;
-
-        let tl = gsap.timeline({
-            repeat: -1,
-            yoyo: true,
-            ease: 'expo.out',
+        videos.forEach((video, i) => {
+            const videoElement = document.getElementById(`video${i + 1}`);
+            if (i === indexRef.current) {
+                videoElement.addEventListener('canplaythrough', () => {
+                    videoElement.play();
+                });
+            } else {
+                videoElement.pause();
+            }
         });
+    }, [videos]);
 
-        gsap.to(overlay, 1, {
-            clipPath: 'polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)',
-            ease: 'expo.out',
-            onComplete: () => (overlay.style.display = 'none'),
-        });
+    const showNextVideo = useCallback(() => showVideo(indexRef.current + 1), [showVideo]);
+    const showPrevVideo = useCallback(() => showVideo(indexRef.current - 1), [showVideo]);
 
-        gsap.set(['#hero-1 h2, #hero-1 h1, #hero-1 h3'], {
-            clipPath: 'polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)',
-        });
-
-        gsap.set(
-            [
-                `#hero-2 h2, #hero-3 h2, #hero-4 h2, #hero-5 h2,
-               #hero-2 h1, #hero-3 h1, #hero-4 h1, #hero-5 h1,
-               #hero-2 h3, #hero-3 h3, #hero-4 h3, #hero-5 h3`,
-            ],
-            {
-                clipPath: 'polygon(0% 0%, 0% 0%, 0% 100%, 0% 100%)',
-            },
-        );
-
-        // Define the correct polygon coordinates
-        const polygonCoords = '0% 0%, 0% 0%, 0% 100%, 0% 100%';
-        while (i < 5) {
-            // Use the correct coordinates for the first set of elements
-            tl.to(`#hero-${i} h2`, 0.9, {
-                clipPath: `polygon(${polygonCoords})`,
-                delay: del,
-            })
-                .to(
-                    `#hero-${i} h1`,
-                    0.9,
-                    {
-                        clipPath: `polygon(${polygonCoords})`,
-                    },
-                    '-=0.3',
-                )
-                .to(
-                    `#hero-${i} h3`,
-                    0.9,
-                    {
-                        clipPath: `polygon(${polygonCoords})`,
-                    },
-                    '-=0.3',
-                )
-                .to(
-                    `#hero-${i} .hi-${i}`,
-                    0.7,
-                    {
-                        clipPath: `polygon(${polygonCoords})`,
-                    },
-                    '-=1',
-                );
-            // Use the correct coordinates for the second set of elements
-            tl.to(`#hero-${i + 1} h2`, 0.9, {
-                clipPath: 'polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)',
-            })
-                .to(
-                    `#hero-${i + 1} h1`,
-                    0.9,
-                    {
-                        clipPath: 'polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)',
-                    },
-                    '-=0.3',
-                )
-                .to(
-                    `#hero-${i + 1} h3`,
-                    0.9,
-                    {
-                        clipPath: 'polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)',
-                    },
-                    '-=0.3',
-                );
-            // Increment the counter
-            i++;
-        }
-    }, []);
+    const renderVideo = useCallback(
+        (video, i) => (
+            <video
+                key={i}
+                src={video.src}
+                className={cx('video', { live: video.isActive })}
+                id={`video${i + 1}`}
+                alt={`Video ${i + 1}`}
+                onEnded={showNextVideo}
+                autoPlay
+                muted
+                preload="auto"
+            />
+        ),
+        [showNextVideo],
+    );
 
     return (
-        <section className={cx('page-wrap')}>
-            <main className={cx('titleSlider')}>
-                <article id="hero-1" style={{ '--i': '5' }}>
-                    <div className={cx('hero-info')}>
-                        <h2>Travel the</h2>
-                        <h1>World</h1>
-                        <h3>Pragser Wildsee, Italy</h3>
-                    </div>
-                    <div className="hero-image hi-1"></div>
-                </article>
-                <article id="hero-2" style={{ '--i': '4' }}>
-                    <div className={cx('hero-info')}>
-                        <h2>Savour the</h2>
-                        <h1>Journey</h1>
-                        <h3>Marignier, France</h3>
-                    </div>
-                    <div className="hero-image hi-2"></div>
-                </article>
-                <article id="hero-3" style={{ '--i': '3' }}>
-                    <div className={cx('hero-info')}>
-                        <h2>Expand Your</h2>
-                        <h1>Horizons</h1>
-                        <h3>Hooker Valley Track, New Zealand</h3>
-                    </div>
-                    <div className="hero-image hi-3"></div>
-                </article>
-                <article id="hero-4" style={{ '--i': '2' }}>
-                    <div className={cx('hero-info')}>
-                        <h2>Explore and</h2>
-                        <h1>Reflect</h1>
-                        <h3>Dolmites, Italy</h3>
-                    </div>
-                    <div className="hero-image hi-4"></div>
-                </article>
-                <article id="hero-5" style={{ '--i': '1' }}>
-                    <div className={cx('hero-info')}>
-                        <h2>Change Your</h2>
-                        <h1>Perspective</h1>
-                        <h3>Phuket, Thailand</h3>
-                    </div>
-                    <div className="hero-image hi-5"></div>
-                </article>
-            </main>
-            <div className="sectionSlider"></div>
+        <section id="home" className={cx('wrapperHome')}>
+            <div className={cx('videos')}>{videos.map(renderVideo)}</div>
+            <div className={cx('videoControls')}>
+                <svg
+                    className={cx('prev')}
+                    data-id="video"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="86.742"
+                    height="73.216"
+                    viewBox="0 0 86.742 73.216"
+                    onClick={showPrevVideo}
+                >
+                    <path d="M22.897 73.216L0 48.158 44.032 0v6.983L27.159 25.498l17.248 18.94L84.897 0v6.985L68.533 24.942l18.209 19.961v7.01L65.35 28.435 47.593 47.931l16.681 18.304v6.981l-40.299-44.22L6.468 48.207l16.429 18.028z"></path>
+                </svg>
+                <h1 className={cx('big-text')}>{countries[indexRef.current]}</h1>
+                <svg
+                    className={cx('next')}
+                    data-id="video"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="86.742"
+                    height="73.216"
+                    viewBox="0 0 86.742 73.216"
+                    onClick={showNextVideo}
+                >
+                    <path d="M63.845 73.216l22.897-25.058L42.71 0v6.983l16.873 18.515-17.248 18.94L1.845 0v6.985l16.364 17.957L0 44.903v7.01l21.392-23.478 17.757 19.496-16.681 18.304v6.981l40.299-44.22 17.507 19.211-16.429 18.028z"></path>
+                </svg>
+            </div>
+
+            {/* Title home */}
+            <div className={cx('contentBox')}>
+                <strong>Explore The World</strong>
+                <h1>The right destination for you and your family</h1>
+                <NavLink to={'/search/'}>
+                    <Button style={{ marginTop: '20px' }} primary>
+                        Explore Now
+                    </Button>
+                </NavLink>
+            </div>
         </section>
     );
-};
+});
 
 export default Banner;
--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,3 +1,7 @@
+import { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import classNames from 'classnames/bind';
 import styles from './Contact.module.scss';
 import Button from '../../shared/Button/Button';
@@ -6,50 +10,87 @@ import InputField from '../../shared/InputField/InputField';
 const cx = classNames.bind(styles);
 
 const Contact = () => {
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+        emailjs.sendForm('service_374175s', 'template_0gdog76', form.current, '6uPsaCp_ILJ7p9IYD').then(
+            (result) => {
+                console.log(result.text);
+                toast.promise(
+                    () =>
+                        new Promise((resolve) => {
+                            setTimeout(() => resolve('Successfully!'), 1500);
+                        }),
+                    {
+                        pending: 'Processing!',
+                        success: 'Successfully!',
+                        error: 'Error!',
+                    },
+                );
+            },
+            (error) => {
+                toast.error(error.text);
+            },
+        );
+    };
     return (
-        <section id="contact" className={cx('container')}>
-            <div className={cx('titleBox')}>
-                <h2>contact</h2>
-                <span>contact for any query</span>
-            </div>
-            <div className={cx('contactBox')}>
-                <form className={cx('contactLeft')}>
-                    <div className={cx('contactFlex')}>
-                        <InputField
-                            id="yourname"
-                            label="Your Name"
-                            type="text"
-                            placeholder="Enter your name."
-                            required
-                        />
-                        <InputField
-                            id="youremail"
-                            label="Your Email"
-                            type="email"
-                            placeholder="Enter your email."
-                            required
-                        />
-                    </div>
-                    <div className={cx('contactColumn')}>
-                        <InputField id="subject" label="Subject" type="text" placeholder="Enter subject." required />
-                        <InputField
-                            textarea
-                            id="message"
-                            label="Message"
-                            placeholder="Enter message."
-                            className="mb-4"
-                            required
-                        />
-                    </div>
-                    <Button type="submit" primary>
-                        Send Message
-                    </Button>
-                </form>
-                <div className={cx('contactRight')}>
-                    <img src={require('../../assets/images/contact.jpg')} alt="about img" />
+        <>
+            <section id="contact" className={cx('container')}>
+                <div className={cx('titleBox')}>
+                    <h2>contact</h2>
+                    <span>contact for any query</span>
                 </div>
-            </div>
-        </section>
+                <div className={cx('contactBox')}>
+                    <form ref={form} onSubmit={sendEmail} className={cx('contactLeft')}>
+                        <div className={cx('contactFlex')}>
+                            <InputField
+                                id="yourname"
+                                label="Your Name"
+                                type="text"
+                                name="user_name"
+                                placeholder="Enter your name."
+                                require
+                            />
+                            <InputField
+                                id="youremail"
+                                label="Your Email"
+                                type="email"
+                                name="user_email"
+                                placeholder="Enter your email."
+                                require
+                            />
+                        </div>
+                        <div className={cx('contactColumn')}>
+                            <InputField
+                                id="subject"
+                                label="Subject"
+                                type="text"
+                                name="user_subject"
+                                placeholder="Enter subject."
+                                require
+                            />
+                            <InputField
+                                textarea
+                                id="message"
+                                label="Message"
+                                name="message"
+                                placeholder="Enter message."
+                                className="mb-4"
+                                require
+                            />
+                        </div>
+                        <Button type="submit" primary>
+                            Send Message
+                        </Button>
+                    </form>
+                    <div className={cx('contactRight')}>
+                        <img src={require('../../assets/images/contact.jpg')} alt="about img" />
+                    </div>
+                </div>
+            </section>
+            <ToastContainer />
+        </>
     );
 };
 
